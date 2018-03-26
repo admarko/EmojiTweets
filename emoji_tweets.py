aeef266c8cd7bae8562a3f858564e5952f24d869
Alex Markowitz <admarko@gmail.com>
2018-03-26 00:58:27 -0500

    Version 2.0
    Major cleanup

--- emoji_tweets.py
+++ emoji_tweets.py
@@ -14,6 +14,51 @@ import tweepy                           # twitter parsing
 from operator import itemgetter         # Dictionary sort
 from tqdm import tqdm                   # progress bar
 
+# Global Variables
+
+MAX_TWEETS = 100;           # Total number of tweets to scrape
+search_query = "test";      # Query to search
+tweets_per_query = 100;     # 100 tweets at once
+international = True;       # False searches domestically
+
+emojistat = 0;
+stat = 0;
+hasLoc = 0;
+
+location_map = {};
+emoji_map = {};
+emoji_count_map = {};
+
+states = {'Alabama','Alaska','Arizona','Arkansas','California','Colorado',
+'Connecticut','Delaware','Florida','Georgia','Hawaii','Idaho','Illinois',
+'Indiana','Iowa','Kansas','Kentucky','Louisiana','Maine','Maryland',
+'Massachusetts','Michigan','Minnesota','Mississippi','Missouri','Montana',
+'Nebraska','Nevada','New Hampshire','New Jersey','New Mexico','New York',
+'North Carolina','North Dakota','Ohio','Oklahoma','Oregon','Pennsylvania',
+'Rhode Island', 'South Carolina','South Dakota','Tennessee','Texas','Utah',
+'Vermont','Virginia','Washington','West Virginia','Wisconsin','Wyoming'};
+
+# Parse command line options
+# parser = argparse.ArgumentParser("python emoji_tweets.py", usage='%(prog)s [-h] [-v] [-t tweets] [-q query] [--d domestic]')
+# parser.add_argument("-v", "--version", help="version number of application", action="store_true")
+# parser.add_argument("-t", "--tweets", type="int", default=100, help="amount of tweets to scrape")
+# parser.add_argument("-q", "--query", type=string??, help="query to search on", metavar='')
+# parser.add_argument("-d", "--domestic", help="international by default", metavar='')
+# args = parser.parse_args()
+
+# if args.version:
+#     print "Version 2.0 of emoji_tweets"
+#     exit()
+
+# if args.domestic:
+#     international = False;
+
+# international = args.international
+# search_query = args.query
+
+
+#API:
+
 # Google Maps API Keys
 #1: "AIzaSyBrsqIQyfG8napKGOAqN-Ukg3HKZBbkdvY" - crashed on 15k - gmaps error
 #2: "AIzaSyD6YCa6XnkdGipnwGUD_31u86PEZd5znjI" - crashed on 14k - gmaps error
@@ -25,8 +70,8 @@ gmaps = googlemaps.Client(key=map_key);
 # Twitter API keys
 consumer_key = 'Oi9h0A02ZOXMIDzoXsy6sWwHl'
 consumer_secret = 'Hh8dBjAP8KTGB2n7gg845WvOXSjm4MlAHntrxbu5DnhTmI8ysi'
-access_token = '959207976199483393-0AOwXscYZyRqLvEToTb8xeHqZP69g8F'
-access_secret = 'wbcqyeOGWvamz0nO0HaRIzcMs6r8yyEzWvLBV75PxjzKS'
+# access_token = '959207976199483393-0AOwXscYZyRqLvEToTb8xeHqZP69g8F'    TODO: Delete this and next line if not needed
+# access_secret = 'wbcqyeOGWvamz0nO0HaRIzcMs6r8yyEzWvLBV75PxjzKS'
 
 auth = tweepy.AppAuthHandler(consumer_key, consumer_secret)
 api = tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
@@ -36,31 +81,6 @@ if (not api):
     sys.exit(-1)
 
 
-# Global Variables
-MAX_TWEETS = 100;
-search_query = "test";
-tweets_per_query = 100;
-international = False;          # False searches domestically
-
-emojistat = 0;
-stat = 0;
-hasLoc = 0;
-
-location_map = {};
-emoji_map = {};
-emoji_count_map = {};
-
-states = {'Alabama','Alaska','Arizona','Arkansas','California','Colorado',
-         'Connecticut','Delaware','Florida','Georgia','Hawaii','Idaho',
-         'Illinois','Indiana','Iowa','Kansas','Kentucky','Louisiana',
-         'Maine','Maryland','Massachusetts','Michigan','Minnesota',
-         'Mississippi','Missouri','Montana','Nebraska','Nevada',
-         'New Hampshire','New Jersey','New Mexico','New York',
-         'North Carolina','North Dakota','Ohio','Oklahoma','Oregon',
-         'Pennsylvania','Rhode Island', 'South Carolina','South Dakota',
-         'Tennessee','Texas','Utah','Vermont','Virginia','Washington',
-         'West Virginia','Wisconsin','Wyoming'};
-
 # pause tweets
 def limit_handled(cursor):
     while True:
@@ -68,6 +88,10 @@ def limit_handled(cursor):
             yield cursor.next()
         except tweepy.RateLimitError:
             time.sleep(10 * 30)
+
+
+
+#Emoji Methods:
 
 # Return true if char is an emoji
 def char_is_emoji(character):
@@ -79,6 +103,10 @@ def status_has_emoji(text):
         if character in emoji.UNICODE_EMOJI:
             return True
     return False
+
+
+
+#Location Methods:
 
 # parse GoogleMaps JSON response
 def getLocation(addr):
@@ -99,11 +127,12 @@ def getLocation(addr):
 
 # Return true if tweet came from valid location as determined by GoogleMaps API
 def status_has_location(status):
-    loc = status.user.location
-    if (loc):
+    initloc = status.user.location
+    if (initloc):
         try:
-            if (gmaps.geocode(loc)):
-                location = getLocation(gmaps.geocode(loc)[0]["address_components"])
+            # TODO: try saving this as a local var: geoloc = gmaps.geocode(loc)
+            if (gmaps.geocode(initloc)):
+                location = getLocation(gmaps.geocode(initloc)[0]["address_components"])
                 if location is not '':
                     insert_location_map(location, status.text);
                     insert_emoji_map(status.text, location);
@@ -115,6 +144,8 @@ def status_has_location(status):
         return False;
 
 
+
+#Map Methods:
 
 # insert country-emoji pair into map
 def insert_location_map(location, status):
@@ -133,7 +164,7 @@ def insert_emoji_map(status, location):
                 emoji_count_map[char] += 1;
             else:
                 emoji_count_map[char] = 1;
-
+            
             if location is not "none":
                 if char in emoji_map:
                     emoji_map[char] += ", {}".format(location);
@@ -141,6 +172,8 @@ def insert_emoji_map(status, location):
                     emoji_map[char] = location;
 
 
+
+#Script Methods:
 
 # Print final report with statistics to console
 def report(stat, emojistat, hasLoc):
@@ -165,7 +198,7 @@ def main():
     global stat
     global emojistat
     global hasLoc
-
+    
     try:
         if international:
             for status in tqdm(limit_handled(tweepy.Cursor(api.search, q=search_query, count=tweets_per_query).items(MAX_TWEETS))):
@@ -195,6 +228,22 @@ def main():
 # Run main program
 main();
 
+
+
+# TODO:
+
+# make Command line interface
+# add timing from tqdm to final report
+# add graphs to final report
+# 'New YorkNew York' error?
+# clump repeated values into array of arrays
+# make web interface
+
+
+
+
+#Graphing Methods:
+
 # # # Bar Chart
 # # labels = 'Brady', 'Gronkowski', 'Amendola'
 # # y_pos = np.arange(len(labels)) #same as range(len(labels))
@@ -215,11 +264,4 @@ main();
 # # plt.show()
 
 
-# TODO
-# make Command line interface
-# add timing from tqdm to final report
-# add graphs to final report
-# 'New YorkNew York' error?
-# clump repeated values into array of arrays
-# put on github
-# make web interface
+
